9d93e5ed66b1d86629e25a8e64d53e869b300662
David Cheng <davidcheng220@gmail.com>
2024-11-24 14:17:52 +0800

    ptt_dataframe edit

--- t.py
+++ t.py
@@ -1,6 +1,20 @@
-v = "hello"
+# v = "hello"
 
-print(len(v))
-# print(v[1])
-for i in v:
-    print(i, end=" ")
+# print(len(v))
+# # print(v[1])
+# for i in v:
+#     print(i, end=" ")
+
+a = "aidjosasdjoaop"
+
+print(a)
+
+a ,b = 1, 4
+
+print(a+b)
+# pyhton 3.6
+print(f"{a} + {b} = ", a+b)
+print()
+
+print("a + b = " ,a+b)
+help('FORMATING')
